eb6a41af6c4c0833a1523f0a0743187260351234
tanner-tjsmedia <tanner@tjs16media.com>
2026-06-16 15:10:14 +0200

    Firebase config keys live

--- firebase-config.js
+++ firebase-config.js
@@ -8,12 +8,12 @@
    ===================================================== */
 
 const FIREBASE_CONFIG = {
-  apiKey:            "PASTE_YOUR_API_KEY_HERE",
-  authDomain:        "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  projectId:         "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket:     "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId:             "PASTE_YOUR_APP_ID_HERE"
+  apiKey:            "AIzaSyBIsusWIMcFpGKeZeP3CemHikOizby2Zro",
+  authDomain:        "fact-royale.firebaseapp.com",
+  projectId:         "fact-royale",
+  storageBucket:     "fact-royale.firebasestorage.app",
+  messagingSenderId: "79646023605",
+  appId:             "1:79646023605:web:7693871d043af940fd03fc"
 };
 
 firebase.initializeApp(FIREBASE_CONFIG);
